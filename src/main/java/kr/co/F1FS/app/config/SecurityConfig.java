444b7bbd2dbd3e0bf2b2b05edf4929a63c08abb4
seablood <dudwnszero99@gmail.com>
2025-03-26 17:46:59 +0900

    A: XSS 설정 추가

--- src/main/java/kr/co/F1FS/app/config/SecurityConfig.java
+++ src/main/java/kr/co/F1FS/app/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,6 +31,7 @@ import static org.springframework.boot.autoconfigure.security.servlet.PathReques
 @Configuration
 @RequiredArgsConstructor
 @EnableMethodSecurity(securedEnabled = true)
+@EnableWebSecurity
 public class SecurityConfig {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final PrincipalDetailsService principalDetailsService;
